96149cef9e822f40ca181d06340318af122b5152
akinadeoluwasegun1234-ui <akinadeoluwasegun1234@gmail.com>
2026-07-18 18:02:24 +0100

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import ta
 
+
+def calculate_indicators(df):
+    """
+    Calculate technical indicators
+    """
+
+    # EMA
+    df["EMA20"] = ta.trend.ema_indicator(df["close"], window=20)
+    df["EMA50"] = ta.trend.ema_indicator(df["close"], window=50)
+
+    # RSI
+    df["RSI"] = ta.momentum.rsi(df["close"], window=14)
+
+    # MACD
+    macd = ta.trend.MACD(df["close"])
+
+    df["MACD"] = macd.macd()
+    df["MACD_SIGNAL"] = macd.macd_signal()
+
+    return df
